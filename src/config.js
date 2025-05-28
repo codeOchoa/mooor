6c3ef98f3009bcdb19d2eb60ad2fd68e2cfb0ccb
Pablo Matias Ochoa <158297956+codeOchoa@users.noreply.github.com>
2025-05-28 15:40:00 -0300

    Update Fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
     apiKey: "AIzaSyB6zxXJIIc3w-tLK8VjKyeDoG1g7wjKDyU",
@@ -10,4 +11,4 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const db = getDatabase(app);
